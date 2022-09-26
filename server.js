c9494a1ed4384741c8cc57cecb3d21007cbe3432
vitorva <vitor.vazafonso@gmail.com>
2022-09-26 16:45:44 +0200

    fetching data

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
 
 app.get('/test', (req, res) => {
     res.json({
@@ -32,7 +39,3 @@ app.get('/test', (req, res) => {
     });
   });
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-    console.log(`listening on ${port}`);
-});
--- src/components/View.vue
+++ src/components/View.vue
@@ -1,5 +1,20 @@
 <script setup>
 
+// Créer des thumbnail vide ici
+
+import { ref, onMounted } from 'vue';
+
+// Appel a l'api
+
+const playlists = ref(null)
+
+onMounted(async () => {
+    const values = await fetch("http://localhost:4000/api/playlists/top/15")
+    const jsonValues = await values.json()
+    playlists.value = jsonValues["data"]
+    console.log("playlists", playlists.value)
+})
+
 </script>
 
 
@@ -9,27 +24,9 @@
         Playlist
     </h1>
     <div class = "container">
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
-        <div class = "thumbnail"></div>
+        <div v-for="playlist in playlists"> 
+            <div class = "thumbnail"></div>
+        </div>
     </div>
 </div>
 
