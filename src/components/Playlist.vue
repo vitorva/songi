a366f0beade67fb7da0fc7f3f6ae089790df16e8
vitorva <vitor.vazafonso@gmail.com>
2022-09-29 14:22:28 +0200

    wip playlist page

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -5,16 +5,30 @@ const props = defineProps({
     id: String
 })
 
-const tracks = ref(null)
+const title = ref("Fetching playlist...");
+const tracks = ref(null);
+const picture = ref(null);
+const creator = ref(null);
+const year = ref(null);
+const nbTracks = ref(null);
+const duration = ref(null);
 
 onMounted(async () => {
     console.log("id", props.id)
     const values = await fetch(`http://localhost:4000/api/playlist/${props.id}`, {
         "method": "GET"
     })
-    const jsonValues = await values.json()
-    tracks.value = jsonValues.tracks.data
-    console.log("tracks", tracks.value)
+    const json = await values.json()
+
+    title.value = json.title
+    tracks.value = json.tracks.data
+    picture.value = json.picture_big
+    creator.value = json.creator.name
+    year.value = new Date(json.creation_date).getFullYear();
+    nbTracks.value = json.nb_tracks;
+    duration.value = Math.round(json.duration / 60); // From seconds to minutes TODO to computed
+
+    console.log("json", json)
 })
 
 </script>
@@ -24,7 +38,34 @@ onMounted(async () => {
         <div>
             <RouterLink to="/">&lt Back to playlists</RouterLink>
         </div>
-        Mon ID est : {{id}}
-        <div v-for="track in tracks">{{track.title}}</div>
+        <div class="header">
+            <img class="picture" :src="picture" alt="Picture">
+            <div>
+                <p>PLAYLIST</p>
+                <h2>{{ title }}</h2>
+                <p>
+                    <span>By</span> {{ creator }}
+                </p>
+                <p>
+                    {{ `${year} • ${nbTracks} songs • ${duration} minutes` }}
+                </p>
+            </div>
+        </div>
+
+        <!-- TRACKITEM -->
+        <div v-for="track in tracks"><img :src="track.album.cover_small" /> {{track.title}} </div>
     </div>
 </template>
+
+<style scoped>
+    .picture {
+      width: 15rem;
+      height: 15rem;
+      margin-right: 2rem;
+    }
+  
+    .header {
+      display: flex;
+      align-items: center;
+    }
+  </style>
