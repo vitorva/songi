458190f431eaf0b5347e2612633ea9e7de41fe59
vitorva <vitor.vazafonso@gmail.com>
2022-10-27 15:44:45 +0200

    serve the src directory

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve the public directory
+app.use(express.static("public"));
+
+// Serve the src directory
+app.use("/src", express.static("src"));
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
+  console.log(`listening on ${port}`);
 });
 
-app.get('/test', (req, res) => {
-    res.json({
-        message: 'Behold The MEVN Stack!'
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Behold The MEVN Stack!",
+  });
+});
+
+// Top playlists
+app.get("/api/playlists/top/:limit", (req, res) => {
+  fetch(
+    "https://api.deezer.com/chart/0/playlists?limit=" + req.params.limit,
+    {}
+  )
+    .then((data) => data.json())
+    .then((json) => {
+      res.send(json);
     });
 });
 
-  // Top playlists
-  app.get('/api/playlists/top/:limit', (req, res) => {
-    fetch('https://api.deezer.com/chart/0/playlists?limit=' + req.params.limit, {
-    })
-    .then(data => data.json())
-    .then(json => {
+// One playlist
+app.get("/api/playlist/:id", (req, res) => {
+  fetch("https://api.deezer.com/playlist/" + req.params.id, {})
+    .then((data) => data.json())
+    .then((json) => {
       res.send(json);
     });
-  });
-
-  // One playlist
-  app.get('/api/playlist/:id', (req, res) => {
-    fetch('https://api.deezer.com/playlist/' + req.params.id, {
-    })
-    .then(data => data.json())
-    .then(json => {
-      res.send(json);
-    });
-  });
-
+});
