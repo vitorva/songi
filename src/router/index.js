21d964f12dc784c3f885cb639a3ccd0244457304
vitorva <vitor.vazafonso@gmail.com>
2022-11-07 15:13:50 +0100

    remove hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
 import Playlist from "../components/Playlist.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
