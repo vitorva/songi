8dfda8cfeb1164351e203848756e06ba332d1eae
vitorva <vitor.vazafonso@gmail.com>
2022-11-08 09:33:30 +0100

    github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+// vite.config.js
+import { ghPages } from "vite-plugin-gh-pages";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), ghPages()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
